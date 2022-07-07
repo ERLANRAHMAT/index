f64e7590710b58d9543e6542669c988df03558ef
Satria1234567890 <kenzipratama495@gmailm.com>
2022-07-07 12:23:40 +0700

    y

---
+++ canvas-tmp/ngetes.js
@@ -0,0 +1,5 @@
+const viko = "ini adalah folder sampah gambar canvas, pastikan output kesini"
+console.log(viko)
+
+// by viko dwi kurniawan- tempplate by zekais
+// saya bukan suhu lo ya awokawowkaowkoakwok jangan sok sok an bisa ngoding aslinya tampal punya orang
